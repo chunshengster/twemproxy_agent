100f60c1345870f464b2a6d3f5578e2e42c761e2
Wang Chunsheng <chunshengster@gmail.com>
2013-11-04 12:07:02 +0800

    add tw control function code ,begin!

--- agent/setting.go
+++ agent/setting.go
@@ -1,9 +1,11 @@
 package agent
 
-//import (
-////"encoding/json"
-////"launchpad.net/goyaml"
-//)
+import (
+	"os"
+
+//"encoding/json"
+//"launchpad.net/goyaml"
+)
 
 //type AgentSeting struct{
 //	AgentCfgServerURI string
@@ -21,17 +23,43 @@ type TwemproxyCft struct {
 }
 
 type TwemproxyInstance struct {
+	TwCFG        *TwemproxyCft
 	PidFile      string
 	TwInitScript string
 	TwStatsPort  uint32
 }
 
-func GetRemoteCfgData() int {
-	return 0
+func checkFile(path string) (bool, err) {
+	_, err := os.Stat(path)
+	return os.IsExist(err), err
 }
 
-func (twI *TwemproxyInstance) ReloadTw() int {
-	return 0
+//FIXME: use nutcracker -t -f file to check new configuration is OK
+func testTWCfg() (bool,err) {
+	return true,nil
+}
+
+func doReloadTW(cmd string)(bool,err){
+	
+	return true,nil
+}
+func (twI *TwemproxyInstance) ReloadTw() (bool, err) {
+	isExist, err := checkFile(twI.TwCFG.TwCFGFile)
+	if isExist != true {
+		//FIXME:do some log and print something
+		return isExist, err
+	}
+	cfgOk,err := testTWCfg()
+	if cfgOk !== true{
+		return cfgOk,err
+	}
+	
+	reloadRe,err := doReloadTW()
+	if reloadRe != true{
+		//FIXME: do some log and print something
+	}
+	
+	return true,nil
 
 }
 
--- agent/util.go
+++ agent/util.go
@@ -1,1 +1,3 @@
 package agent
+
+func Debug()
